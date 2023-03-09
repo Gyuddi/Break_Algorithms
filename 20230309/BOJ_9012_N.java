2f1e07b993967c6df7f1924504200cc3f055984c
nakjun <111031253+nakjun12@users.noreply.github.com>
2023-03-09 09:09:14 +0900

    [Solved✌🏻]낙준_괄호

---
+++ 20230309/BOJ_9012_N.java
@@ -0,0 +1,29 @@
+var input = require("fs")
+  .readFileSync("20230309/9012.txt")
+  .toString()
+  .split("\n");
+
+let result = true;
+
+for (let i = 1; i <= input[0]; i++) {
+  result = true;
+  if (input[i].length % 2 === 1) {
+    result = false;
+  }
+
+  if (result) {
+    while (input[i].indexOf("()") !== -1) {
+      input[i] = input[i].replace("()", "");
+    }
+    if (input[i].length > 0) {
+      result = false;
+    }
+  }
+  if (result) {
+    result = "YES";
+  } else {
+    result = "NO";
+  }
+
+  console.log(result);
+}
