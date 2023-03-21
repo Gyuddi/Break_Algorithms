cd4f30f83ec7f5ffe221a5d28574f0e53649e1f8
Gyuddi <90381760+Gyuddi@users.noreply.github.com>
2023-03-21 23:57:14 +0900

    [Solved✌🏻]규현_가위 바위 보

---
+++ 20230321/INF_02_03.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class INF_02_03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] a = new int[N];
+        int[] b = new int[N];
+        for (int i = 0; i < N; i++) {
+            a[i] = sc.nextInt();
+        }
+        for (int i = 0; i < N; i++) {
+            b[i] = sc.nextInt();
+        }
+
+        String answer = "";
+
+        for (int i = 0; i < N; i++) {
+            if (a[i] == b[i]) answer += "D";
+            else if (a[i] == 1 && b[i] == 3) answer += "A";
+            else if (a[i] == 2 && b[i] == 1) answer += "A";
+            else if (a[i] == 3 && b[i] == 2) answer += "A";
+            else answer += "B";
+        }
+        for (int i = 0; i < answer.length(); i++) {
+            System.out.println(answer.charAt(i));
+        }
+    }
+}
