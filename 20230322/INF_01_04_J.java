c46b73a4afee7030243f135278ef2901ff736418
injongjeon <111412215+jnjongjeon@users.noreply.github.com>
2023-03-23 20:11:03 +0900

    [Solved✌🏻]단어 뒤집기

---
+++ 20230322/INF_01_04_J.java
@@ -0,0 +1,40 @@
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class INF_01_04_J {
+    public ArrayList<String> solution(int n, String[] str){
+        ArrayList<String> answer = new ArrayList<>();
+//        for(String x : str){
+//            String tmp = new StringBuilder(x).reverse().toString();
+//            answer.add(tmp);
+//        }
+        for(String x : str){
+            char[] s = x.toCharArray();
+            int lt = 0;
+            int rt = x.length()-1;
+            while(lt<rt){
+                char tmp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = tmp;
+                lt++;
+                rt--;
+            }
+            String tmp = String.valueOf(s); // char[] -> String으로 변환
+            answer.add(tmp);//ArrayList에 추가.
+        }
+        return answer;
+    }
+    public static void main(String[] args){
+        Main T = new Main();
+        Scanner kb=new Scanner(System.in);
+        int n = kb.nextInt();
+        String[] str = new String[n];
+        for(int i=0; i<n; i++){
+            str[i] = kb.next();
+        }
+        for(String x : T.solution(n, str)){
+            System.out.println(x);
+        }
+    }
+}
