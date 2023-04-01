a7b02b804e37bcca79eda85e78e6c79f3a60b190
Gyuddi <90381760+Gyuddi@users.noreply.github.com>
2023-04-01 21:27:09 +0900

    [Solved✌🏻]규현_두 배열 합치기(two pointers algorithm)

---
+++ 20230401/INF_03_01.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class INF_03_01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arrA = new int[N];
+        for (int i = 0; i < N; i++) {
+            arrA[i] = sc.nextInt();
+        }
+        int M = sc.nextInt();
+        int[] arrB = new int[M];
+        for (int i = 0; i < M; i++) {
+            arrB[i] = sc.nextInt();
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+        int p1, p2 = 0;
+        while ()
+    }
+}
